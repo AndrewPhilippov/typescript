70890f0231b27eb8dbf631a2b40484c17c9f947f
Andrew <a@fandb.pro>
2020-02-23 07:40:55 +0300

    Walked through tuple and enum

--- day3/app.ts
+++ day3/app.ts
@@ -2,9 +2,34 @@
 //   name: string;
 //   age: number;
 // } = {
+
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN , READ_ONLY, AUTHOR};
+
 const person = {
-  name: 'Maximilian',
-  age: 30
+  name: 'Andrew',
+  age: 33,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
 };
 
+if (person.role === Role.AUTHOR) {
+  console.log('Is author')
+}
+// person.role.push('admin');
+// person.role[1] = 10; // !!! ERR
+// person.role = [0, 'admin', 'theceo']; // !!! ERR
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
