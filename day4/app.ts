a68b6e63048f07c7a6525d8349eeeeed072df016
andrewphilippov <a@fandb.pro>
2020-07-07 08:29:04 +0300

    Unknown type is better than <any> but have to use conditional check to assign value to strict type

--- day4/app.ts
+++ day4/app.ts
@@ -1,14 +1,22 @@
-function add(n1: number, n2: number): number {
+const add = (n1: number, n2: number): number => {
   return n1 + n2
 }
 
-function printResult(num: number):void {
+const printResult = (num: number):void => {
   console.log(`Result ${num}`)
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
   const result = n1 + n2
   cb(result)
 }
 
 addAndHandle(5, 6, printResult)
+
+let userInput: unknown
+let userName: string
+
+userInput = 'Andrew'
+if (typeof userInput === 'string') {
+  userName = userInput
+}
