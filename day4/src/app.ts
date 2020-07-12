7a5749a0be8c57ffbac86cc34e75be2caf72bc92
andrewphilippov <a@fandb.pro>
2020-07-12 23:38:59 +0300

    -\__/-

--- day4/src/app.ts
+++ day4/src/app.ts
@@ -1,27 +1,17 @@
-const add = (n1: number, n2: number): number => {
-  return n1 + n2
+const button = document.querySelector('button')
+
+function add(n1: number, n2: number) {
+    debugger
+    if (n1 + n2 > 0) {
+        return n1 + n2
+    }
+    return
 }
 
-const printResultDay4 = (num: number):void => {
-  console.log(`Result ${num}`)
+function clickHandler(message: string) {
+    console.log('clicked', message)
 }
 
-const addAndHandleDay4 = (n1: number, n2: number, cb: (num: number) => void) => {
-  const result = n1 + n2
-  cb(result)
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, 'bom-booooom'))
 }
-
-addAndHandleDay4(5, 6, printResultDay4)
-
-let userInput: unknown
-let userName: string
-
-userInput = 'Andrew'
-if (typeof userInput === 'string') {
-  userName = userInput
-}
-function generateString( message: string, code: number ): never {
-  throw { message, errorCode: code }
-}
-
-generateString('an error occurred', 500)
